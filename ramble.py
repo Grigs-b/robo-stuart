6cc14c2de95a4e170e450892a50ec84c47d4141d
Ryan Grigsby <rgrigsby@inovasolutions.com>
2014-12-19 17:22:12 -0500

    stuart now rambles correctly after browsing talkbass

--- ramble.py
+++ ramble.py
@@ -4,7 +4,7 @@ import pyttsx, random, time, wikipedia
 # in seconds
 RAMBLE_FREQUENCY_MIN = 30
 RAMBLE_FREQUENCY_MAX = 60
-
+WORDS_PER_MINUTE = 140
 
 def boot_stuart():
     engine = pyttsx.init()
@@ -14,7 +14,7 @@ def boot_stuart():
             print("*clears throat*")
             #engine.setProperty('voice', voice.id)
     # sloooww down
-    engine.setProperty('rate', engine.getProperty('rate')-60)
+    engine.setProperty('rate', WORDS_PER_MINUTE)
     return engine
 
 def get_starting_point(length):
@@ -34,21 +34,18 @@ def get_rant(summary):
     start = get_starting_point(len(sentences))
     return '. '.join(sentences[start:])
 
-
-engine = boot_stuart()
 # TODO : Only ramble during work hours.
 while True:
     try:
+        engine = boot_stuart()
         topic = wikipedia.random()
         print("I'm reading about {}".format(topic))
         rant = get_rant(wikipedia.summary(wikipedia.random()))
         engine.say(rant)
         engine.runAndWait()
-        if not engine.isBusy():
-            print("Time to browse some TalkBass")
-            time.sleep(random.randint(RAMBLE_FREQUENCY_MIN, RAMBLE_FREQUENCY_MAX))
+        print("Time to browse some TalkBass")
+        time.sleep(random.randint(RAMBLE_FREQUENCY_MIN, RAMBLE_FREQUENCY_MAX))
     except Exception as err:
         print("This computer is...messing up.")
         print(str(err))
         engine.stop()
-        engine = boot_stuart()
